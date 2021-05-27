984bf0c1fc2b115ee030da324942bea3951822d2
Frederick-dev23 <f.p.n.97230@gmail.com>
2021-05-27 16:00:16 -0400

    final comit

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-    apiKey: "AIzaSyA1paKAoX2jh8Lf-DdLkXuTtHdY6ozMeM8",
-    authDomain: "marvel-quiz-bb14f.firebaseapp.com",
-    projectId: "marvel-quiz-bb14f",
-    storageBucket: "marvel-quiz-bb14f.appspot.com",
-    messagingSenderId: "528458864181",
-    appId: "1:528458864181:web:2b7857a15d911e5cb3037c"
+  
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
 
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer-container">
-                <p>Project réaliser par Frédérick Ponch - 2021</p>
+                <p>Project réaliser par Frédérick Ponchateau - 2021</p>
                 <p>Les icônes Wolverine, Iron-man, Spider-man, batman sont prise sur iconFinder.com</p>
             </div>
         </footer>
